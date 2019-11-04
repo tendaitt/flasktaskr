812d6888743e594ea7a0137716c7deaf5cb7330c
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-04 11:54:05 +0200

    add login and user registration forms.

--- project/forms.py
+++ project/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, IntegerField, SelectField, \
+    PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class AddTaskForm(FlaskForm):
     task_id = IntegerField()
@@ -18,3 +19,32 @@ class AddTaskForm(FlaskForm):
         ]
     )
     status = IntegerField('Status')
+
+class RegisterForm(FlaskForm):
+    name = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo('password', \
+            message='Passwords must match')]
+    )
+
+class LoginForm(FlaskForm):
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
