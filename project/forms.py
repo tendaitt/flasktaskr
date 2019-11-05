d6532146677a754841a9ca7e050bf718639a05aa
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-06 00:59:59 +0200

    add email validation to RegisterForm class.

--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField, SelectField, \
     PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class AddTaskForm(FlaskForm):
     task_id = IntegerField()
@@ -27,7 +27,7 @@ class RegisterForm(FlaskForm):
     )
     email = StringField(
         'Email',
-        validators=[DataRequired(), Length(min=6, max=40)]
+        validators=[DataRequired(), Email(), Length(min=6, max=40)]
     )
     password = PasswordField(
         'Password',
