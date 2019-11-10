d92632de465128af054d2a560f4d7b7f6ff4632b
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-11 00:05:41 +0200

    refresh database.

--- db_create.py
+++ db_create.py
@@ -1,13 +1,18 @@
-from views import db
-from models import Task
+from project import db
+from project.models import Task, User
 from datetime import date
 
 # create the database and the db table
 db.create_all()
 
 # insert data
-db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
-db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
+db.session.add(User("admin", "ad@min.com", "admin", "admin"))
+db.session.add(
+    Task("Finish this tutorial", date(2015, 3, 13), 10, date(2015, 2, 13), 1, 1)
+)
+db.session.add(
+    Task("Finish Real Python", date(2015, 3, 13), 10, date(2015, 2, 13), 1, 1)
+)
 
 # commit the changes
 db.session.commit()
