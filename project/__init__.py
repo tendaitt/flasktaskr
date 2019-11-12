a85576c9a4ab95a342868bfb70521055044c06df
tendaitt <970497+tendaitt@users.noreply.github.com>
2019-11-12 18:47:18 +0200

    add error logging and update debug mode.

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -16,8 +17,21 @@ app.register_blueprint(tasks_blueprint)
 
 @app.errorhandler(404)
 def not_found(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write(f"\n404 error at {current_timestamp}: {r} ")
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write(f"\n500 error at {current_timestamp}: {r} ")
     return render_template('500.html'), 500
--- project/test_main.py
+++ project/test_main.py
@@ -16,10 +16,13 @@ class MainTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
+
+        self.assertEquals(app.debug, False)
     
     def tearDown(self):
         db.session.remove()
@@ -41,21 +44,6 @@ class MainTests(unittest.TestCase):
         response = self.app.get('/this-route-does-not-exist')
         self.assertEquals(response.status_code, 404)
         self.assertIn(b'Sorry. There\'s nothing here.', response.data)
-    
-    def test_500_error(self):
-        bad_user = User(
-            name='Jeremy',
-            email='jeremy@realpython.com',
-            password='django'
-        )
-        db.session.add(bad_user)
-        db.session.commit()
-        self.assertRaises(ValueError, self.login, 'Jeremy', 'django')
-        try:
-            response = self.login('Jeremy', 'django')
-            self.assertEquals(response.status_code, 500)
-        except:
-            pass
 
 if __name__ == "__main__":
     unittest.main()
